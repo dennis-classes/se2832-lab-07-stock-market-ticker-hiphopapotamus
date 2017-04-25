459bbba667ba01de7ec26a982e211652e75adcb9
bensonja <bensonja@msoe.edu>
2017-04-25 16:21:13 -0500

    Added first few tests. Added method that speeds up setting the currentQuote

--- test/StockQuoteAnalyzerTest.java
+++ test/StockQuoteAnalyzerTest.java
@@ -1,21 +1,14 @@
+import exceptions.InvalidAnalysisState;
 import exceptions.InvalidStockSymbolException;
 import exceptions.StockTickerConnectionError;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-
-
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
 import org.mockito.Mock;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.DataProvider;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.testng.Assert.assertEquals;
 
 public class StockQuoteAnalyzerTest {
     @Mock
@@ -40,5 +33,63 @@ public class StockQuoteAnalyzerTest {
     @Test(expectedExceptions = InvalidStockSymbolException.class)
     public void constructorShouldThrowExceptionWhenSymbolIsInvalid() throws Exception {
         analyzer = new StockQuoteAnalyzer("ZZZZZZZZZ", generatorMock, audioMock);
+    }
+
+    @Test(expectedExceptions = StockTickerConnectionError.class)
+    public void refreshShouldThrowExceptionWhenItCannotObtainAQuote() throws Exception {
+        analyzer = new StockQuoteAnalyzer("AAC", generatorMock, audioMock);
+        when(generatorMock.getCurrentQuote()).thenThrow(Exception.class);
+        analyzer.refresh();
+    }
+
+    @Test
+    public void refreshShouldNotThrowAnExceptionWhenItCanObtainAQuote() throws Exception {
+        analyzer = new StockQuoteAnalyzer("AAC", generatorMock, audioMock);
+        setGeneratorToAddQuote("red", 0, 0, 0);
+        analyzer.refresh();
+    }
+
+    @Test
+    public void getSymbolShouldReturnCorrectSymbolWhenCalled() throws Exception {
+        analyzer = new StockQuoteAnalyzer("AAC", generatorMock, audioMock);
+        assertEquals("AAC", analyzer.getSymbol());
+    }
+
+    @Test(expectedExceptions = InvalidAnalysisState.class)
+    public void getCurrentPriceShouldThrowExceptionWhenCurrentQuoteIsNull() throws Exception {
+        analyzer = new StockQuoteAnalyzer("AAC", generatorMock, audioMock);
+        analyzer.getCurrentPrice();
+    }
+
+    @Test(expectedExceptions = InvalidAnalysisState.class)
+    public void getCurrentPriceReturnLastTradedValueWhenCurrentQuoteIsNotNull() throws Exception {
+        analyzer = new StockQuoteAnalyzer("AAC", generatorMock, audioMock);
+        setGeneratorToAddQuote("red", 100, 0, 0);
+        analyzer.refresh();
+        assertEquals(analyzer.getCurrentPrice(), 100, .0001);
+    }
+
+    private void setGeneratorToAddQuote(String symbol, double lastTrade, double close, double change) throws Exception {
+        when(generatorMock.getCurrentQuote()).thenReturn(new StockQuoteInterface() {
+            @Override
+            public String getSymbol() {
+                return symbol;
+            }
+
+            @Override
+            public double getLastTrade() {
+                return lastTrade;
+            }
+
+            @Override
+            public double getClose() {
+                return close;
+            }
+
+            @Override
+            public double getChange() {
+                return change;
+            }
+        });
     }
 }
